b99030f1768628722c1c5d11d89672876546ae9b
Filip Černý <145854819+fidulin@users.noreply.github.com>
2024-03-20 20:26:29 +0100

    fixed kodLetiste (navchar2(3))

--- xfukal01_xcerny81.sql
+++ xfukal01_xcerny81.sql
@@ -11,7 +11,7 @@ create table PremiovyUcet (
 );
 
 create table Letiste (
-    idLetiste int primary key,
+    kodLetiste nvarchar2(3) primary key,
     nazev nvarchar2(255) not null,
     mesto nvarchar2(255) not null,
     stat nvarchar2(255) not null
@@ -30,11 +30,11 @@ create table Let (
     pocetMist int not null,
 
     idSpolecnosti int,
-    idLetiste_prilet int,
-    idLetiste_odlet int,
+    kodLetiste_prilet nvarchar2(3),
+    kodLetiste_odlet nvarchar2(3),
     CONSTRAINT fk_idSpolecnosti FOREIGN KEY (idSpolecnosti) REFERENCES LeteckaSpolecnost(idSpolecnosti)  ON DELETE CASCADE,
-    CONSTRAINT fk_idLetiste_prilet FOREIGN KEY (idLetiste_prilet) REFERENCES Letiste(idLetiste)  ON DELETE CASCADE,
-    CONSTRAINT fk_idLetiste_odlet FOREIGN KEY (idLetiste_odlet) REFERENCES Letiste(idLetiste)  ON DELETE CASCADE
+    CONSTRAINT fk_kodLetiste_prilet FOREIGN KEY (kodLetiste_prilet) REFERENCES Letiste(kodLetiste)  ON DELETE CASCADE,
+    CONSTRAINT fk_kodLetiste_odlet FOREIGN KEY (kodLetiste_odlet) REFERENCES Letiste(kodLetiste)  ON DELETE CASCADE
 );
 
 create table Letenka (
@@ -57,17 +57,18 @@ INSERT INTO Ucet (idUctu, jmeno, prijmeni, rokNarozeni) VALUES
 INSERT INTO PremiovyUcet (idUctu, sleva) VALUES
 (1, 10);
 
-INSERT INTO Letiste (idLetiste, nazev, mesto, stat) VALUES
-(1, 'Letiště Václava Havla Praha', 'Praha', 'Česká republika');
+INSERT INTO Letiste (kodLetiste, nazev, mesto, stat) VALUES
+('PRG', 'Letiště Václava Havla Praha', 'Praha', 'Česká republika');
 
-INSERT INTO Letiste (idLetiste, nazev, mesto, stat) VALUES
-(2, 'Letiště Václava Havla Praha', 'Brno', 'Česká republika');
+INSERT INTO Letiste (kodLetiste, nazev, mesto, stat) VALUES
+('BRQ', 'Letiště Turany', 'Brno', 'Česká republika');
 
 INSERT INTO LeteckaSpolecnost (idSpolecnosti, nazev, zemePusobeni, reditel) VALUES
 (1, 'Czech Airlines', 'Česká republika', 'Petr Nový');
 
-INSERT INTO Let (idLetu, typLetadla, pocetMist, idSpolecnosti, idLetiste_prilet, idLetiste_odlet) VALUES
-(1, 'Airbus A320', 180, 1, 1, 2);
+INSERT INTO Let (idLetu, typLetadla, pocetMist, idSpolecnosti, kodLetiste_prilet, kodLetiste_odlet) VALUES
+(1, 'Airbus A320', 180, 1, 'PRG', 'BRQ');
 
 INSERT INTO Letenka (idLetenky, cena, trida, sedadlo, jmeno, prijmeni, idUctu, idLetu) VALUES
 (1, 1000, 'Economy', 1, 'Jan', 'Novák', 1, 1);
+
