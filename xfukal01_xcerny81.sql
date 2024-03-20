4f7175eec64c7dab31fd1c82fe4b4e795cbbdb07
Filip Černý <145854819+fidulin@users.noreply.github.com>
2024-03-20 21:25:10 +0100

    added automatic generation of idUctu

--- xfukal01_xcerny81.sql
+++ xfukal01_xcerny81.sql
@@ -1,5 +1,7 @@
+
 create table Ucet (
-    idUctu int primary key,
+    --id Uctu generovano automaticky
+    idUctu number generated always as identity primary key,
     jmeno nvarchar2(255) not null,
     prijmeni nvarchar2(255) not null,
     rokNarozeni int not null
@@ -36,9 +38,9 @@ create table Let (
     typLetadla nvarchar2(255) not null,
     pocetMist int not null,
 
-    ICO int,
-    kodLetiste_prilet nvarchar2(3),
-    kodLetiste_odlet nvarchar2(3),
+    ICO int not null,
+    kodLetiste_prilet nvarchar2(3) not null,
+    kodLetiste_odlet nvarchar2(3) not null,
     CONSTRAINT fk_ICO FOREIGN KEY (ICO) REFERENCES LeteckaSpolecnost(ICO)  ON DELETE CASCADE,
     CONSTRAINT fk_kodLetiste_prilet FOREIGN KEY (kodLetiste_prilet) REFERENCES Letiste(kodLetiste)  ON DELETE CASCADE,
     CONSTRAINT fk_kodLetiste_odlet FOREIGN KEY (kodLetiste_odlet) REFERENCES Letiste(kodLetiste)  ON DELETE CASCADE
@@ -48,22 +50,24 @@ create table Letenka (
     idLetenky int primary key,
     cena int not null,
     trida nvarchar2(255) not null,
+    CONSTRAINT EnumClass CHECK (trida in ('Economy', 'Business', 'First Class')),
     sedadlo int not null,
     jmeno nvarchar2(255),
     prijmeni nvarchar2(255),
 
     idUctu int,
-    idLetu int not null ,
+    idLetu int not null,
     CONSTRAINT fk_idUctu FOREIGN KEY (idUctu) REFERENCES Ucet(idUctu) ON DELETE CASCADE,
     CONSTRAINT fk_idLetu FOREIGN KEY (idLetu) REFERENCES let(idLetu) ON DELETE CASCADE
 );
 
 -- testovaci naplneni databaze
-INSERT INTO Ucet (idUctu, jmeno, prijmeni, rokNarozeni) VALUES
-(1, 'Jan', 'Novák', 1985);
+INSERT INTO Ucet (jmeno, prijmeni, rokNarozeni) VALUES
+('Jan', 'Novák', 1985);
 
-INSERT INTO PremiovyUcet (idUctu, sleva) VALUES
-(1, 80);
+INSERT INTO Ucet (jmeno, prijmeni, rokNarozeni) VALUES
+('Petr', 'Pavel', 1970);
+
 
 INSERT INTO Letiste (kodLetiste, nazev, mesto, stat) VALUES
 ('PRG', 'Letiště Václava Havla Praha', 'Praha', 'Česká republika');
@@ -77,8 +81,8 @@ INSERT INTO LeteckaSpolecnost (ICO, nazev, zemePusobeni, reditel) VALUES
 INSERT INTO Let (idLetu, typLetadla, pocetMist, ICO, kodLetiste_prilet, kodLetiste_odlet) VALUES
 (1, 'Airbus A320', 180, 12345678, 'PRG', 'BRQ');
 
-INSERT INTO Letenka (idLetenky, cena, trida, sedadlo, jmeno, prijmeni, idUctu, idLetu) VALUES
-(1, 1000, 'Economy', 1, 'Jan', 'Novák', 1, 1);
+INSERT INTO Letenka (idLetenky, cena, trida, sedadlo, jmeno, prijmeni, idLetu) VALUES
+(1, 1000, 'Economy', 1, 'Jakub', 'Horuba', 1);
 
 
 
