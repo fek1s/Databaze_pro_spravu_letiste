0103f203e1b06b597a0013bc68052cc55a138a2f
Filip Černý <145854819+fidulin@users.noreply.github.com>
2024-03-20 17:55:34 +0100

    Added connections and fixed existing connections
    idk lol

--- xfukal01_xcerny81.sql
+++ xfukal01_xcerny81.sql
@@ -1,11 +1,3 @@
-
-drop table Ucet;
-drop table PremiovyUcet;
-drop table Letentka;
-drop table Let;
-drop table Letiste;
-drop table LeteckaSpolecnost;
-
 create table Ucet (
     idUctu int primary key,
     jmeno nvarchar2(255) not null,
@@ -16,21 +8,6 @@ create table Ucet (
 create table PremiovyUcet (
     idUctu int primary key REFERENCES Ucet(idUctu) ON DELETE CASCADE,
     sleva int not null
-);
-
-create table Letentka (
-    idLetentky int primary key,
-    cena int not null,
-    trida nvarchar2(255) not null,
-    sedadlo int not null,
-    jmeno nvarchar2(255),
-    prijmeni nvarchar2(255),
-);
-
-create table Let (
-    idLetu int primary key,
-    typLetadla nvarchar2(255) not null,
-    pocetMist int not null
 );
 
 create table Letiste (
@@ -47,8 +24,30 @@ create table LeteckaSpolecnost (
     reditel nvarchar2(255) not null
 );
 
-create table LetenkaUctu (
-    idUctu int REFERENCES Ucet(idUctu) ,
-    idLetentky int REFERENCES Letentka(idLetentky)
+create table Let (
+    idLetu int primary key,
+    typLetadla nvarchar2(255) not null,
+    pocetMist int not null,
 
+    idSpolecnosti int,
+    idLetiste_prilet int,
+    idLetiste_odlet int,
+    CONSTRAINT fk_idSpolecnosti FOREIGN KEY (idSpolecnosti) REFERENCES LeteckaSpolecnost(idSpolecnosti)  ON DELETE CASCADE,
+    CONSTRAINT fk_idLetiste_prilet FOREIGN KEY (idLetiste_prilet) REFERENCES Letiste(idLetiste)  ON DELETE CASCADE,
+    CONSTRAINT fk_idLetiste_odlet FOREIGN KEY (idLetiste_odlet) REFERENCES Letiste(idLetiste)  ON DELETE CASCADE
 );
+
+create table Letenka (
+    idLetenky int primary key,
+    cena int not null,
+    trida nvarchar2(255) not null,
+    sedadlo int not null,
+    jmeno nvarchar2(255),
+    prijmeni nvarchar2(255),
+
+    idUctu int,
+    idLetu int,
+    CONSTRAINT fk_idUctu FOREIGN KEY (idUctu) REFERENCES Ucet(idUctu) ON DELETE CASCADE,
+    CONSTRAINT fk_idLetu FOREIGN KEY (idLetu) REFERENCES let(idLetu) ON DELETE CASCADE
+);
+
