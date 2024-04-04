f335f3528f43f15a7358346ef81116261776a121
Jakub Fukala <xfukal01@vutbr.cz>
2024-04-04 16:00:31 +0200

    feat: add data

--- xfukal01_xcerny81.sql
+++ xfukal01_xcerny81.sql
@@ -156,7 +156,7 @@ WHERE EXISTS(
     SELECT *
     FROM Letenka LE
     WHERE L.idLetu = LE.idLetu
-)
+);
 
 -- Dotaz 4: vypisuje vsechny cestujici kteri leti s leteckeckou společností Ryanair
 SELECT L.jmeno , L.prijmeni
